4b3e8e37f4d251c5a738530ec88ff11ad0e7abc4
Bruno Barros <brunog.barros@pm.me>
2022-12-04 15:27:21 -0300

    scanf

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    // entradas do teclado
+    int num1, num;
+    printf("Entre com o valor: ");
+    scanf("%d", &num);
+    printf("Numero digitado: %d", num);
     return 0;
 }
