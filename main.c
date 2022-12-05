cde11cbb2c638cc9be65c34ffea2226f73bdef72
Bruno Barros <brunog.barros@pm.me>
2022-12-04 21:38:18 -0300

    funcoes parte 1

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
+
+// Prototipo de funções
+
+void linhas();
+
+
 int main() {
-    // entradas do teclado
-    int num1, num;
-    printf("Entre com o valor: ");
-    scanf("%d", &num);
-    printf("Numero digitado: %d", num);
-    return 0;
+    linhas();
+    printf("\nMenu principal do programa\n\n");
+    linhas();
+    printf("\nMenu principal do programa 2\n\n");
+}
+
+void linhas() {
+    for(int i =0;i<3;i++){
+        for(int j=0;j<30;j++)
+            putchar('#');
+        putchar('\n');
+    }
 }
